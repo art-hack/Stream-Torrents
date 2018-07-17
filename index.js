2428cddf0d6e0dce6905d5df72dda33a540d11b4
Art-hack <17ucs036@lnmiit.ac.in>
2018-07-18 01:10:01 +0530

    Added some content

--- index.js
+++ index.js
@@ -2,10 +2,11 @@
 var express = require('express');
 var app = express();
 app.set('port', process.env.PORT || 5000);
+app.set("view engine", "ejs");
 
 // Our first route
 app.get('/', function (req, res) {
-  res.send('Lets Begin');
+  res.render("home");
 });
 
 // Listen to port
